6c2dcae84279da31e99c331eebc7634ccf294421
Tácito Henrique <tacito.graca@hp.com>
2023-05-14 23:23:42 -0300

    api simples

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,31 +1,25 @@
 from flask import Flask, jsonify, request
-from app.domain.user import User
+from app.domain.user.User import create_user, get_user, get_all_users, update_user, delete_user
 from app.infrastructure.database.sqlite import db
 
-user = User()
 
 def register_routes(app):
-    
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
-        
     
     @app.route('/users', methods=['POST'])
     def create():
         data = request.get_json()
         name, email = data['name'], data['email']
-        user = user.create_user(name, email)
+        user = create_user(name, email)
         return jsonify({'user': str(user)}), 201
 
     @app.route('/users', methods=['GET'])
     def get_all():
-        users = user.get_all_users()
+        users = get_all_users()
         return jsonify({'users': [str(user) for user in users]})
 
     @app.route('/users/<int:user_id>', methods=['GET'])
     def get_one(user_id):
-        user = user.get_user(user_id)
+        user = get_user(user_id)
         if user:
             return jsonify({'user': str(user)})
         return jsonify({'error': 'User not found'}), 404
@@ -34,14 +28,14 @@ def register_routes(app):
     def update(user_id):
         data = request.get_json()
         name, email = data.get('name'), data.get('email')
-        user = user.update_user(user_id, name, email)
+        user = update_user(user_id, name, email)
         if user:
             return jsonify({'user': str(user)})
         return jsonify({'error': 'User not found'}), 404
 
     @app.route('/users/<int:user_id>', methods=['DELETE'])
     def delete(user_id):
-        user = user.delete_user(user_id)
+        user = delete_user(user_id)
         if user:
             return jsonify({'result': 'User deleted'})
         return jsonify({'error': 'User not found'}), 404
@@ -59,7 +53,7 @@ if __name__ == '__main__':
 
     @app.route('/users')
     def index():
-        users = User.query.order_by(User.id).all()
+        users = query.order_by(id).all()
         if len(users) == 0:
             abort(404)    
             return jsonify({
@@ -69,7 +63,7 @@ if __name__ == '__main__':
             
     @app.route('/users')
     def index():
-        users = User.query.order_by(User.id).all()
+        users = query.order_by(id).all()
         if len(users) == 0:
             abort(404)    
             return jsonify({
@@ -85,10 +79,10 @@ if __name__ == '__main__':
         age = body.get('age', None)    
         try:
             user = User(name=name, age=age)
-            user.create()        
+            create()        
             return jsonify({
                 'success': True,
-                'created': user.format()
+                'created': format()
             })
         except:
             abort(422)
@@ -96,13 +90,13 @@ if __name__ == '__main__':
     #show  
     @app.route('/users/<int:user_id>')
     def show(user_id):
-        user = User.query.filter(User.id == user_id).one_or_none()
+        user = query.filter(id == user_id).one_or_none()
         if user is None:
             abort(404)
         else:
             return jsonify({
                 'success': True,
-                'user': user.format()
+                'user': format()
             })
 
     #update
@@ -110,15 +104,15 @@ if __name__ == '__main__':
     def update(user_id):
         body = request.get_json()    
         try:
-            user = User.query.filter(User.id == user_id).one_or_none()
+            user = query.filter(id == user_id).one_or_none()
             if user is None:
                 abort(404)        
                 if 'name' in body:
-                    user.name = body.get('name')        
-                    user.update()        
+                    name = body.get('name')        
+                    update()        
                     return jsonify({
                         'success': True,
-                        'updated': user.format()
+                        'updated': format()
                     })
         except:
             abort(422)
@@ -127,13 +121,13 @@ if __name__ == '__main__':
     @app.route('/users/<int:user_id>', methods=['DELETE'])
     def destroy(user_id):
         try:
-            user = User.query.filter(User.id == user_id).one_or_none()
+            user = query.filter(id == user_id).one_or_none()
             if user is None:
                 abort(404)        
-                user.delete()        
+                delete()        
                 return jsonify({
                     'success': True,
-                    'deleted': user.format()
+                    'deleted': format()
             })
         except:
             abort(422)
