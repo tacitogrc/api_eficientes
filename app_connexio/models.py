90d33ce20131c2d4864d15c3a6fbce12460a4f65
Tácito Henrique <tacito.graca@hp.com>
2023-05-16 18:39:51 -0300

    automatização para a criação de banco de dados automático

--- app_connexio/models.py
+++ app_connexio/models.py
@@ -17,6 +17,16 @@ class PersonSchema(ma.SQLAlchemyAutoSchema):
         model = Person
         load_instance = True
         sqla_session = db.session
+        
+        
+class Note(db.Model):
+    __tablename__ = "note"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
 person_schema = PersonSchema()
 people_schema = PersonSchema(many=True)
